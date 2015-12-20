b12bed222f8715e6804284c53b7ce3ae66120ad7
Luke Gruber <luke.gru@gmail.com>
2015-12-20 17:07:07 -0500

    get tests passing for minitest 5.0.8

--- lib/covet/test_runners/minitest.rb
+++ lib/covet/test_runners/minitest.rb
@@ -20,7 +20,9 @@ module Covet
         rescue LoadError
           require 'minitest/unit' # minitest 4
         end
-        version = defined?(::Minitest::VERSION) ? ::Minitest::VERSION : ::Minitest::Unit::VERSION
+        version = defined?(::Minitest::VERSION) ?
+          ::Minitest::VERSION : # v >= 5
+          ::Minitest::Unit::VERSION # v < 5
 
         if version.to_f >= 5.0
           ::Minitest.after_run do
@@ -38,6 +40,7 @@ module Covet
             end
           end
 
+          if ::Minitest::Runnable.method_defined?(:run_one_method) # minitest > 5.0.8
           ::Minitest::Runnable.class_eval do
             @@covet_run_num = 0
             @@covet_skips = 0
@@ -128,11 +131,122 @@ module Covet
                 result
               # NOTE: if the interrupt is fired outside of `Minitest.run_one_method`, then the
               # collection file gets logged to even on interrupt :(
-              rescue Interrupt
+              rescue Interrupt, SystemExit
                 Covet::TestRunners::Minitest.create_collection_file_on_exit = false
                 raise
               end
 
+            end
+          end
+          else
+            # minitest ~ 5.0.8
+            ::Minitest::Runnable.class_eval do
+              @@covet_run_num = 0
+              @@covet_skips = 0
+              @@covet_failures = 0
+              @@covet_start_time = nil
+              class << self
+                def covet_start_time
+                  @@covet_start_time
+                end
+
+                alias :covet_old_run :run
+
+                def run(reporter, options = {})
+                  filter = options[:filter] || '/./'
+                  filter = Regexp.new $1 if filter =~ /\/(.*)\//
+                  filtered_methods = self.runnable_methods.find_all { |m|
+                    filter === m || filter === "#{self}##{m}"
+                  }
+
+                  failures = 0
+                  with_info_handler reporter do
+                    filtered_methods.each do |method_name|
+
+                      # first run, collect coverage 'base' coverage information
+                      # (coverage information for before any tests get run).
+                      if @@covet_run_num == 0
+                        @@covet_start_time = Time.now
+                        base_coverage = CovetCoverage.peek_result
+                        base_coverage = Covet.normalize_coverage_info(base_coverage)
+                        if base_coverage.empty?
+                          warn "Warning - covet is not properly set up, as it must be required " \
+                            "before other libraries to work correctly.\nTry adding\n  require 'covet'\n" \
+                            "to the top of your test helper file."
+                        end
+                        Covet::BASE_COVERAGE.update base_coverage
+                        Covet.log_collection << ['base', base_coverage, {
+                          :version => Covet::VERSION,
+                          :options => Covet.options,
+                          :seed => Random::DEFAULT.seed,
+                        }]
+                      end
+
+                      @@covet_run_num += 1
+                      file = nil
+                      begin
+                        file = instance_method(method_name).source_location[0]
+                      rescue
+                        warn "\nWarning - Skipping collecting test coverage for method #{klass}##{method_name}\n"
+                        next
+                      end
+
+                      before = CovetCoverage.peek_result
+
+                      # Run test method
+                      before_t = Time.now
+                      result = self.new(method_name).run
+                      after_t = Time.now
+                      raise "#{self}#run _must_ return self" unless self === result
+                      ret = reporter.record result
+
+                      if result.skipped?
+                        @@covet_skips += 1
+                        next
+                      end
+
+                      # test failed, don't record coverage info
+                      new_failures = result.failures.size
+                      if new_failures > failures
+                        @@covet_failures += 1
+                        failures += 1
+                        next
+                      end
+
+                      after = CovetCoverage.peek_result
+
+                      before_orig = Covet.normalize_coverage_info(before)
+                      if Covet::BASE_COVERAGE.any?
+                        before = Covet.diff_coverages(Covet::BASE_COVERAGE, before_orig)
+                      end
+
+                      after_orig = Covet.normalize_coverage_info(after)
+                      after = Covet.diff_coverages(before_orig, after_orig)
+                      if @@covet_run_num > 1
+                        if [:random_seeded, :ordered].include?(Covet.options[:test_order])
+                          Covet::BASE_COVERAGE.update(after_orig)
+                        end
+                      end
+
+                      if after == before
+                        after = nil
+                      end
+
+                      Covet.log_collection << ["#{file}##{method_name}", after, {
+                        :time => sprintf("%.2f", after_t - before_t),
+                      }]
+
+                      ret
+                    end
+                  end
+                # NOTE: if the interrupt is fired outside of `Minitest.run_one_method`, then the
+                # collection file gets logged to even on interrupt :(
+                rescue Interrupt, SystemExit
+                  Covet::TestRunners::Minitest.create_collection_file_on_exit = false
+                  raise
+                end
+
+              end
             end
           end
         else
@@ -239,7 +353,7 @@ module Covet
               result
             # NOTE: if the interrupt is fired outside of `Minitest::Unit::TestCase.run`, then the
             # collection file gets logged even on interrupt :(
-            rescue Interrupt
+            rescue Interrupt, SystemExit
               Covet::TestRunners::Minitest.create_collection_file_on_exit = false
               raise
             end
